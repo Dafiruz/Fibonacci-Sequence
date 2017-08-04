165ebd8ff352523bcf736bb36448e06274456b03
Dafiruz <m4gn3t46@gmail.com>
2017-08-04 21:12:06 +0100

    final version

--- Fibonacci Sequence/Program.cs
+++ Fibonacci Sequence/Program.cs
@@ -10,7 +10,29 @@ namespace Fibonacci_Sequence
     {
         static void Main(string[] args)
         {
+            int max, number = 0, previous = 1, second_previous = 1;
 
+            Console.WriteLine("Welcome to the Fibonacci Sequence!");
+            Console.WriteLine();
+            Console.WriteLine("Would you like to see the Fibonacci Sequence until what number?");
+            int.TryParse(Console.ReadLine(), out max);
+            Console.WriteLine("---------");
+
+            Console.WriteLine(number);
+            Console.WriteLine(previous);
+            Console.WriteLine(second_previous);
+            do
+            {
+                number = previous + second_previous;
+                second_previous = previous;
+                previous = number;
+                Console.WriteLine(number);
+            } while (number < max);
+
+            Console.WriteLine("---------");
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Exit!");
+            Console.ReadKey();
         }
     }
 }
